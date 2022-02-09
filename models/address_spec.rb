8d63e8a3a6284648a76e5c3973a44f3b3a096747
kei-kamiguchi <56471454+kei-kamiguchi@users.noreply.github.com>
2022-02-09 12:59:31 +0900

    Update address_spec.rb

--- models/address_spec.rb
+++ models/address_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Address, type: :model do
   describe "associations" do
-    it { should belong_to(:addressable) }
+    it { should belong_to(:addressable).class_name('Address') }
     it { should have_many(:orders) }
   end
   
